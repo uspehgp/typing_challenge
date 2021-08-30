09d9707d690417382907df5a0e95b264e032d9d6
uspehgp <uspehgp@gmail.com>
2021-08-30 10:20:08 +0300

    create view and some logic compare typed text

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {lorem} from 'faker'
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'typing';
+  randomText = lorem.sentence();
+  enteredText = ''
+
+  onInput(value: string) {
+    this.enteredText = value;
+  }
 }
